dc3c665d1dd4f2e8b725b5ec0fbceac4e1f431d8
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-02-11 13:52:27 -0600

    Fixed typos and made a few indents to Book.java

--- Assignment1/Assignment1 Week1/src/entities/Book.java
+++ Assignment1/Assignment1 Week1/src/entities/Book.java
@@ -2,27 +2,39 @@ package entities;
 
 public class Book {
     private final String id;
-    private  final String title;
+    private final String title;
     private final String author;
-    private final Boolean available;
-}
+    private Boolean available; // should not be final, as the availability of the book will change.
 
-public Book(String id, String title, String author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.available = true;
-}
+    public Book(String id, String title, String author, Boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
 
-public String getId() {return id;}
-public String getTitle() {return title;}
-public String getAuthor() {return author;}
+    public String getId() {
+        return id;
+    }
 
-public boolean isAvailable() {return available;}
-public void setAvailable(boolean available) { this.available = available; }
+    public String getTitle() {
+        return title;
+    }
 
-@Override
-public  String toString() {
-    return "Book{id='" + id + "', title='" + title + "', author='" + author + "', available=" + available + "}";
-}
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{ id='" + id + "', title='" + title + "', author='" + author + "', available=" + available + "}";
+    }
 }

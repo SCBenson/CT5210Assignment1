ae67e321587eba0da53841f80fa8bb4d383db917
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-02-13 13:13:50 -0600

    Added copies and isbn parameters to the Book class, new method to get the number of copies. Need to create a method to change availability based on the number of copies.

--- Assignment1/Assignment1 Week1/src/entities/Book.java
+++ Assignment1/Assignment1 Week1/src/entities/Book.java
@@ -4,12 +4,16 @@ public class Book {
     private final String id;
     private final String title;
     private final String author;
+    private final String isbn;
+    private int copies;
     private Boolean available; // should not be final, as the availability of the book will change.
 
-    public Book(String id, String title, String author, Boolean available) {
+    public Book(String id, String title, String author, String isbn, int copies, Boolean available) {
         this.id = id;
         this.title = title;
         this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
         this.available = true;
     }
 
@@ -25,8 +29,16 @@ public class Book {
         return author;
     }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getAvailableCopies() {
+        return copies;
+    }
+
     public boolean isAvailable() {
-        return available;
+        return available; // need to refactor to check if copies is non-zero and flip availability if not.
     }
 
     public void setAvailable(boolean available) {

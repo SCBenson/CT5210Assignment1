2f48d2f8a52c0954478e0a9b7786929d5310a1b5
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2026-02-13 14:09:44 -0600

    Casted the List return from the interface (best practive) to an ArrayList.

--- Assignment1/Assignment1 Week1/src/controller/LibraryController.java
+++ Assignment1/Assignment1 Week1/src/controller/LibraryController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import entities.Book;
 import frameworks.InMemoryLibraryRepository;
 import usecases.*;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class LibraryController {
     private final InMemoryLibraryRepository repository;
@@ -18,6 +22,26 @@ public class LibraryController {
         this.listBookUseCase = new ListBookUseCase(repository);
         this.registerMemberUseCase = new RegisterMemberUseCase(repository);
         this.returnBookUseCase = new ReturnBookUseCase(repository);
+    }
+
+    private void addBook(String title, String author, String isbn, int copies) {
+        try {
+            Book book = new Book(title, author, isbn, copies);
+            addBookUseCase.execute(book);
+            System.out.println("Book added Successfully: " + title);
+        } catch (Exception e) {
+            System.err.println("Error adding book: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<Book> listAllBooks() {
+        try {
+            List<Book> books = listBookUseCase.execute();
+            return new ArrayList<>(books);
+        } catch (Exception e) {
+            System.err.println("Error listing books: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
 
 }
